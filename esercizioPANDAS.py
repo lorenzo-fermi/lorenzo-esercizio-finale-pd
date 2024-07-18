7894470588699eb36701ca9d769d16b436934fd0
lorenzo-fermi <lorenzo.cristiano.fermi@gmail.com>
2024-07-18 15:11:47 +0200

    Update esercizioPANDAS.py

--- esercizioPANDAS.py
+++ esercizioPANDAS.py
@@ -1,69 +1,66 @@
 import pandas as pd
 import numpy as np
 
-# Funzione per generare e esplorare i dati
-def caricamento_Esplorazione():
-    np.random.seed(42)  # Per riproducibilità
+class Dati:
+    def __init__(self, n_clienti=10):
+        self.n_clienti = n_clienti
+        
+    
+    def generate_data(self):
+        eta = np.random.randint(18, 80, size=self.n_clienti)
+        durata_abbonamento = np.random.randint(1, 72, size=self.n_clienti)  # Durata abbonamento in mesi
+        tariffa_mensile = np.random.randint(20, 100, size=self.n_clienti)  # Tariffa mensile in euro, ora intera
+        dati_consumati = np.random.randint(1, 100, size=self.n_clienti)  # Dati consumati in GB, ora interi
+        servizio_clienti_contatti = np.random.randint(0, 10, size=self.n_clienti)
+        churn = np.random.choice([True, False], size=self.n_clienti, p=[0.8, 0.2])  # 80% non hanno lasciato, 20% hanno lasciato
 
-    n_clienti = 10  # Numero di clienti
-    eta = np.random.randint(18, 80, size=n_clienti)
-    durata_abbonamento = np.random.randint(1, 72, size=n_clienti)  # Durata abbonamento in mesi
-    tariffa_mensile = np.random(20, 100, size=n_clienti)  # Tariffa mensile in euro
-    dati_consumati = np.random(1, 100, size=n_clienti)  # Dati consumati in GB
-    servizio_clienti_contatti = np.random.randint(0, 10, size=n_clienti)
-    churn = np.random.choice([True, False], size=n_clienti, p=[0.8, 0.2])  # 80% non hanno lasciato, 20% hanno lasciato
+        data = {
+            'ID_Clienti': np.arange(1, self.n_clienti + 1),
+            'Età': eta,
+            'Durata_Abbonamento': durata_abbonamento,
+            'Tariffa_Mensile': tariffa_mensile,
+            'Dati_Consumati': dati_consumati,
+            'Servizio_Clienti_Contatti': servizio_clienti_contatti,
+            'Churn': churn
+        }
 
-    # Creazione del DataFrame
-    data = {
-        'ID_Clienti': np.arange(1, n_clienti + 1),
-        'Età': eta,
-        'Durata_Abbonamento': durata_abbonamento,
-        'Tariffa_Mensile': tariffa_mensile,
-        'Dati_Consumati': dati_consumati,
-        'Servizio_Clienti_Contatti': servizio_clienti_contatti,
-        'Churn': churn
-    }
+        df = pd.DataFrame(data)
+        df.to_csv('clienti.csv', index=False)
+        
+        return df
+    
+    def esplorazione_dati(self, df):
+        print("ECCO LE INFO")
+        df.info()
+        print("\nECCO IL DESCRIBE")
+        print(df.describe())
+        print("\nECCO IL VALUECOUNTS")
+        print(df.value_counts())
 
-    df = pd.DataFrame(data)
+class PuliziaDati:
+    def __init__(self, df):
+        self.df = df
+    
+    def pulisci_dati(self):
+        print("Valori anomali in 'Età':")
+        print(self.df[self.df['Età'] < 0])
 
-    # Salvataggio del DataFrame in un file CSV
-    df.to_csv('clienti.csv', index=False)
+        print("\nValori anomali in 'Tariffa_Mensile':")
+        print(self.df[self.df['Tariffa_Mensile'] > 100])
 
-    # Visualizzazione del DataFrame
-    print("ECCO LE INFO")
-    df.info()
-    print("\nECCO IL DESCRIBE")
-    print(df.describe())
-    print("\nECCO IL VALUECOUNTS")
-    print(df.value_counts())
+        self.df.loc[self.df['Età'] < 0, 'Età'] = self.df['Età'].mean()
+        self.df.loc[self.df['Tariffa_Mensile'] > 100, 'Tariffa_Mensile'] = 100
 
-    return df
+        print("\nDataFrame corretto:")
+        print(self.df)
 
-# Funzione per pulire i dati
-def pulizia_dati(df):
-    # Identificazione delle anomalie
-    print("Valori anomali in 'Età':")
-    print(df[df['Età'] < 0])
+# Utilizzo delle classi per generare, esplorare e pulire i dati
+data_gen = Dati()
+df = data_gen.generate_data()
+data_gen.esplorazione_dati(df)
 
-    print("\nValori anomali in 'Tariffa_Mensile':")
-    print(df[df['Tariffa_Mensile'] > 100])
-
-    # Correzione delle anomalie
-    # Per 'Età' negativa, possiamo sostituirla con il valore assoluto o una media plausibile
-    df.loc[df['Età'] < 0, 'Età'] = df['Età'].mean()
-
-    # Per 'Tariffa_Mensile' troppo alta, possiamo impostarla a un valore massimo ragionevole (es. 100)
-    df.loc[df['Tariffa_Mensile'] > 100, 'Tariffa_Mensile'] = 100
-
-    # Visualizzazione del DataFrame corretto
-    print("\nDataFrame corretto:")
-    print(df)
-
-# Generazione ed esplorazione del dataset
-df = caricamento_Esplorazione()
-
-# Pulizia dei dati
-pulizia_dati(df)
+data_cleaner = PuliziaDati(df)
+data_cleaner.pulisci_dati()
 
 
 
